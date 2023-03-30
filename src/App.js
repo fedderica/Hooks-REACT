45d1674814e7c66191763a466f37e56c4ceb918a
fedderica <fedderica.bartoli@gmail.com>
2023-03-30 09:24:24 -0300

    REACT-HOOK-PRUEBAPRIMERCOMPONENTE

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Lifecycle from './Lifecycle';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>APP</h1>
+    <Lifecycle/>
+    </div>
+  );
+}
+
+export default App;
