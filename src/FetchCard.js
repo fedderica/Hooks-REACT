b0a6fe62542963ef875d6f125bbd0a97b60c9cf2
fedderica <fedderica.bartoli@gmail.com>
2023-03-31 10:27:22 -0300

    final-commit

--- src/FetchCard.js
+++ src/FetchCard.js
@@ -6,8 +6,14 @@ const initialUser = {
     email: "correo@correo.com"
 }
 
+const initalPosts = [
+    {id: 1, title: "Post 1"},
+    {id: 1, title: "Post 2"},
+]
+
 const FetchCard = () => {
     const [user, setUser] = useState(initialUser);
+    const [posts, setsPosts] = useState([initialPosts]);
 
     const updateUser = () => {
        getUser()
@@ -22,9 +28,23 @@ const FetchCard = () => {
 
     return (
         <div>
+            <button onClick={updateUser}> 
+                Another User
+            </button>
+
             <h1>{user.name}</h1>
             <h2>{user.email}</h2>
-        </div>
+
+            <br/>
+
+            <h2> Posts - user: {user.id} </h2>
+            <ul>
+                {posts.map(post => (
+                    <li>{post.title}</li>
+                ))}
+                <li> Titulo </li>
+                </ul>
+                </div>
     )
 }
 
